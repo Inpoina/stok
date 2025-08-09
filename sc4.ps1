fa84024fc554e1f28cec17c8ec62db602196c6ac
Inpoina <140089365+Inpoina@users.noreply.github.com>
2025-08-09 14:06:07 +0700

    Add files via upload

--- sc4.ps1
+++ sc4.ps1
@@ -1,10 +1,10 @@
-# === SETUP PATH ===
-$folderPath = "C:\kampret\"
+
+$folderPath = "C:\Users\kasir_induk\contanak\"
 $dataFiles = Get-ChildItem -Path $folderPath -File
 
 $tail = @()
 
-# === Ambil 415 karakter terakhir dari tiap file ===
+
 foreach ($file in $dataFiles) {
     $content = Get-Content $file.FullName -Raw
     $length = $content.Length
@@ -15,30 +15,26 @@ foreach ($file in $dataFiles) {
     }
 }
 
-# === Regex: Ambil nilai dari "SecaraTunai" ===
+
 $matches = $tail | Select-String -Pattern 'SecaraTunai"?:["]?(\d+(?:\.\d+)?)' -AllMatches |
     ForEach-Object { $_.Matches.Groups[1].Value }
 
-# === Hitung total ===
 $total = 0
 foreach ($num in $matches) {
     $total += [double]$num
 }
 
-# Format total (bulatkan)
 $formattedTotal = [math]::Round($total)
 $strukCount = $matches.Count
 
-# Format data ke bentuk: 102700n5
 $dataToEncrypt = "$formattedTotal" + "n" + "$strukCount"
 
-# === INPUT PASSWORD TANPA TAMPIL ===
 $securePass = Read-Host "p" -AsSecureString
 $passUnsecure = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
     [Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass)
 )
 
-# === AES ENKRIPSI ===
+
 $sha = [System.Security.Cryptography.SHA256]::Create()
 $key = $sha.ComputeHash([Text.Encoding]::UTF8.GetBytes($passUnsecure))
 $iv = New-Object Byte[] 16  # 16 byte nol
@@ -52,7 +48,6 @@ $plainBytes = [Text.Encoding]::UTF8.GetBytes($dataToEncrypt)
 $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
 $base64 = [Convert]::ToBase64String($encryptedBytes)
 
-# Simpan ke file
 $base64 | Set-Content "$folderPath\final.enc"
 
 exit
