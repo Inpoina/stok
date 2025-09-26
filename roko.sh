9571a9662fc5890a1f4798d5628241cad0815f89
Inpoina <140089365+Inpoina@users.noreply.github.com>
2025-09-26 15:28:53 +0700

    Add files via upload

---
+++ roko.sh
@@ -0,0 +1,32 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Jalankan nn.js di Alpine, copy stok.html ke penyimpanan internal Android
+
+# Lokasi rootfs Alpine
+ALPINE_ROOT="/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/alpine"
+ALPINE_FILE="$ALPINE_ROOT/root/stok/stok.html"
+
+# Lokasi tujuan di Android internal storage
+TARGET_DIR="/data/data/com.termux/files/home"
+TARGET_FILE="$TARGET_DIR/index.html"
+# Pastikan folder target ada
+mkdir -p "$TARGET_DIR"
+
+# Jalankan nn.js di Alpine dengan kode toko (param $1)
+proot-distro login alpine -- bash -c "cd /root/stok && node rokofi.js $1"
+
+# Tunggu sebentar agar file selesai dibuat
+sleep 2
+
+# Copy hasil stok.html ke /storage/emulated/0/kampret/
+if [ -f "$ALPINE_FILE" ]; then
+    cp "$ALPINE_FILE" "$TARGET_FILE"
+    echo "✅ stok.html dipindahkan ke $TARGET_FILE"
+
+    # Buka otomatis di browser Android
+    php -S localhost:8080 & am start -a android.intent.action.VIEW -d "http://localhost:8080/"
+
+
+
+else
+    echo "❌ stok.html tidak ditemukan di Alpine"
+fi
